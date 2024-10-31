394e1c252fb813a1094abb0463e5d439c8dfc882
Simon Due Kristensen <simon.99@live.dk>
2024-10-31 12:06:14 +0100

    fix nature - fix trainers (kinda)

--- dist/calc/items.d.ts
+++ dist/calc/items.d.ts
@@ -4,7 +4,7 @@ export declare const SEED_BOOSTED_STAT: {
 };
 export declare function getItemBoostType(item: string | undefined): "Electric" | "Psychic" | "Water" | "Normal" | "Fighting" | "Flying" | "Poison" | "Ground" | "Rock" | "Bug" | "Ghost" | "Steel" | "Fire" | "Grass" | "Ice" | "Dragon" | "Dark" | "Fairy" | undefined;
 export declare function getBerryResistType(berry: string | undefined): "Electric" | "Psychic" | "Water" | "Normal" | "Fighting" | "Flying" | "Poison" | "Ground" | "Rock" | "Bug" | "Ghost" | "Steel" | "Fire" | "Grass" | "Ice" | "Dragon" | "Dark" | "Fairy" | undefined;
-export declare function getFlingPower(item?: string): 0 | 100 | 10 | 50 | 30 | 60 | 85 | 110 | 20 | 90 | 40 | 80 | 130 | 95 | 70;
+export declare function getFlingPower(item?: string): 0 | 10 | 30 | 100 | 40 | 60 | 50 | 85 | 20 | 130 | 110 | 95 | 90 | 80 | 70;
 export declare function getNaturalGift(gen: Generation, item: string): {
     t: TypeName;
     p: number;
